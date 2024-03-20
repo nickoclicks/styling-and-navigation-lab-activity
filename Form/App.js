bd52f7b283b9a2b65baa28042f70df83c597cbe1
nickoclicks <nickocajes@gmail.com>
2024-03-20 21:37:39 +0800

    new

--- Form/App.js
+++ Form/App.js
@@ -1,5 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import * as React from "react";
+import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../Form/src/homescreen";
@@ -7,9 +7,14 @@ import LoginScreen from "./src/login";
 import SignupScreen from "./src/signup";
 import RecoveryScreen from "./src/recovery";
 import LandingScreen from "./src/landing";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+const Drawer = createDrawerNavigator();
+const Stack = createNativeStackNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
-  const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -17,9 +22,35 @@ export default function App() {
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Signup" component={SignupScreen} />
         <Stack.Screen name="Recovery" component={RecoveryScreen} />
-        <Stack.Screen name="Landing" component={LandingScreen} />
+        <Stack.Screen
+          name="Landing"
+          component={MyDrawer}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
+  );
+}
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={LandingScreen} />
+      <Tab.Screen name="Notification" component={LoginScreen} />
+      <Tab.Screen name="Text" component={RecoveryScreen} />
+    </Tab.Navigator>
+  );
+}
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Home" component={MyTabs} />
+      <Drawer.Screen name="Services" component={HomeScreen} />
+      <Drawer.Screen name="Filters" component={RecoveryScreen} />
+      <Drawer.Screen name="About Us" component={LandingScreen} />
+      <Drawer.Screen name="Contact" component={SignupScreen} />
+    </Drawer.Navigator>
   );
 }
 
